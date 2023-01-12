04c072e26f5465cd56645593583ac06e576a5b8a
Ludovic Dumas <ludoloops@pm.me>
2023-01-12 05:31:05 -0500

    feat: chain prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,9 +6,9 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
-const basePromptPrefix = ""
+const basePromptPrefix = "convert the  url to an markdown link: "
+// const secondPrompt = 'use the word from the array to write, make a markdown title, write a small description with them'
 const generateAction = async (req, res) => {
-  // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
@@ -20,7 +20,29 @@ const generateAction = async (req, res) => {
 
   const basePromptOutput = baseCompletion.data.choices.pop()
 
-  res.status(200).json({ output: basePromptOutput })
+  console.log('basePromptOutput', basePromptOutput.text)
+  const secondPrompt =
+    `
+  use the word from the array:  ${req.body.userInput}
+  to write, make a markdown title, on a new line write a small description with them.
+  then put this link: ${basePromptOutput.text} below.
+  `
+
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.7,
+    // I also increase max_tokens.
+    max_tokens: 250,
+  })
+
+  // Get the output
+
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop()
+
+  res.status(200).json({ output: secondPromptOutput })
 }
 
 export default generateAction
